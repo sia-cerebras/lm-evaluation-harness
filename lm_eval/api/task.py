081e1a6c2d6e2113159f317c1544e90814571f52
William <william.marshall@cerebras.net>
2024-02-02 12:21:26 -0800

    Add brier score to multimedqa tasks

--- lm_eval/api/task.py
+++ lm_eval/api/task.py
@@ -1063,6 +1063,9 @@ class ConfigurableTask(Task):
 
             pred = np.argmax(lls)
             pred_norm = np.argmax(lls / completion_len)
+            np_lls = np.array(lls)
+            np_lls -= np_lls.max()
+            pred_probs = np.exp(lls) / np.sum(np.exp(lls))
 
             if self.multiple_input:
                 gold = self.doc_to_text(doc)
@@ -1098,6 +1101,12 @@ class ConfigurableTask(Task):
                 acc_norm = 1.0 if pred_norm == gold else 0.0
                 # TODO: this gets score of 0 on arc_challenge for pythia-70m. need to test that this works properly
                 exact_match = int(is_greedy[gold]) if gold != -100 else 0
+                if gold == -100:
+                    brier = 0
+                else:
+                    labels = np.zeros_like(pred_probs)
+                    labels[gold] = 1
+                    brier = np.mean(np.square(pred_probs - labels))
 
             result_dict = {
                 **({"acc": acc} if "acc" in use_metric else {}),
@@ -1105,6 +1114,7 @@ class ConfigurableTask(Task):
                 **({"mcc": (gold, pred)} if "mcc" in use_metric else {}),
                 **({"acc_norm": acc_norm} if "acc_norm" in use_metric else {}),
                 **({"exact_match": exact_match} if "exact_match" in use_metric else {}),
+                **({"brier_score": brier} if "brier_score" in use_metric else {}),
             }
 
             if "acc_mutual_info" in use_metric:
